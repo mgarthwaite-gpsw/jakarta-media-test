ff4350a9ae92200a28116d1f724e9df4a362f097
Michael Garthwaite <mgarthwaite@mbr-29255.gopro.lcl>
2016-07-19 16:45:46 -0700

    Added a simple log parser

--- mediatest.py
+++ mediatest.py
@@ -5,13 +5,56 @@ import time
 import shutil
 import sys
 
+##smurtlogging
+##find the suite that failed
+##look in the log following the suite name
+##for ever FAIL, pull FAIL line and the line previous to it.
+##repeat for all suites, all iterations
+
+def parseLog(iterationOfLoop):
+    breakString = " tests -"
+    parsedLog = []
+    with open("runLog%i.log" % (iterationOfLoop), "r") as file:
+        text = file.readlines()
+        for line in range(len(text) - 1, -1, -1):
+            if (text[line][0:6] == "Total:"):
+                for appLine in range(line, len(text)):
+                    parsedLog.append(text[appLine])
+    lenParsedLog = len(parsedLog)
+    line = 0
+    while line < lenParsedLog:
+        if "FAIL SUITE: suite_" in parsedLog[line]:
+            stringList = parsedLog[line].split()
+            suiteName = stringList[len(stringList)-1]
+            print suiteName
+            runLogLine = 0
+            for i in range(runLogLine,len(text)):
+                runLogLine += 1
+                if suiteName in text[i]:
+                    break
+            while runLogLine < len(text):
+                if breakString in text[runLogLine]:
+                    runLogLine += 1
+                    break
+                elif "FAIL test_" in text[runLogLine]:
+                    parsedLog.insert(line+1,"\t \t%s" % text[runLogLine])
+                    parsedLog.insert(line+1,"\t \t%s" % text[runLogLine-1])
+                    runLogLine += 1
+                    lenParsedLog = len(parsedLog)
+                else:
+                    runLogLine +=1
+        line += 1
+    completeLog = open("ParsedLog%i.log" % iterationOfLoop, "w+")
+    for line in parsedLog:
+        completeLog.write(line)
 
 
 def runWinTest(pictureList,videoList,iterationOfLoop):
-    if os.path.exists("TestNum%i" % iterationOfLoop): print "Directory exist"
+    if os.path.exists("TestNum%i" % iterationOfLoop):
+        shutil.copy2("gpsdk_jakarta_unittest.exe", "TestNum%i" % iterationOfLoop)
     else:
         os.makedirs("TestNum%i" % iterationOfLoop)
-    shutil.copy2("gpsdk_jakarta_unittest.exe", "TestNum%i" % iterationOfLoop)
+        shutil.copy2("gpsdk_jakarta_unittest.exe", "TestNum%i" % iterationOfLoop)
 
     if iterationOfLoop >= len(pictureList):
         shutil.copy(pictureList[0], "TestNum%i\\tmp.jpg" % iterationOfLoop)
@@ -35,10 +78,11 @@ def runWinTest(pictureList,videoList,iterationOfLoop):
 
 
 def runPosixTest(pictureList, videoList, iterationOfLoop):
-    if os.path.exists("./TestNum%i" % iterationOfLoop): print "Directory exist"
+    if os.path.exists("./TestNum%i" % iterationOfLoop):
+        shutil.copy2("gpsdk_jakarta_unittest.exe", "TestNum%i" % iterationOfLoop)
     else:
         os.makedirs("TestNum%i" % iterationOfLoop)
-    shutil.copy2("gpsdk_jakarta_unittest", "TestNum%i" % iterationOfLoop)
+        shutil.copy2("gpsdk_jakarta_unittest", "TestNum%i" % iterationOfLoop)
 
     if iterationOfLoop >= len(pictureList):
         shutil.copy(pictureList[0], "./TestNum%i/tmp.jpg" % iterationOfLoop)
@@ -53,14 +97,9 @@ def runPosixTest(pictureList, videoList, iterationOfLoop):
         shutil.copy(videoList[iterationOfLoop], "./TestNum%i/tmp.mp4" % iterationOfLoop)
         videoID = iterationOfLoop
     time.sleep(10)
-
     os.chdir("TestNum%i/" % iterationOfLoop)
     time.sleep((((iterationOfLoop*2)/3)%10)+1)
-    if (os.name == "posix"):
-        os.system("./gpsdk_jakarta_unittest -v -j qa -dcomp 511 -dlevel 555 > runLog%i.log 2>&1" % (iterationOfLoop))
-    else:
-        os.system("gpsdk_jakarta_unittest -v -j qa -dcomp 511 -dlevel 555 > runLog%i.log 2>&1" % (iterationOfLoop))
-
+    os.system("./gpsdk_jakarta_unittest -v -j qa -dcomp 511 -dlevel 555 > runLog%i.log 2>&1" % (iterationOfLoop))
     os.system("echo 'PID:%s TestNum%i completed using %s AND %s' >> runLog%i.log" % (os.getpid(), iterationOfLoop , videoList[videoID], pictureList[pictID], iterationOfLoop))
     os.chdir("..")
     return None
@@ -79,32 +118,28 @@ class JDKLibTest():
         return pictureList, videoList
 
     def aggregatePosixLogs(self, listCount):
-        self.aggregateLog = []
+        self.parsedLog = []
         for i in range(0,listCount):
-            with open("./TestNum%i/runLog%i.log" % (i,i),"r") as file:
+            with open("./TestNum%i/ParsedLog%i.log" % (i,i),"r") as file:
                 text = file.readlines()
-                for line in range(len(text)-1,-1,-1):
-                    if (text[line][0:6] == "Total:"):
-                        for appLine in range(line,len(text)):
-                            self.aggregateLog.append(text[appLine])
+                for line in range(0,len(text)):
+                    self.parsedLog.append(text[line])
             file.close()
         completeLog = open("AggregatedLog.log","w+")
-        for line in self.aggregateLog:
+        for line in self.parsedLog:
             completeLog.write(line)
         return completeLog
 
     def aggregateWinLogs(self, listCount):
-        self.aggregateLog = []
+        self.parsedLog = []
         for i in range(0,listCount):
-            with open("..\TestNum%i\\runLog%i.log" % (i,i),"r") as file:
+            with open("TestNum%i\\runLog%i.log" % (i,i),"r") as file:
                 text = file.readlines()
-                for line in range(len(text)-1,-1,-1):
-                    if (text[line][0:6] == "Total:"):
-                        for appLine in range(line,len(text)):
-                            self.aggregateLog.append(text[appLine])
+                for line in range(0,len(text)):
+                    self.parsedLog.append(text[line])
             file.close()
         completeLog = open("AggregatedLog.log","w+")
-        for line in self.aggregateLog:
+        for line in self.parsedLog:
             completeLog.write(line)
         return completeLog
 
